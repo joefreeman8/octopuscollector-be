7e7c73cdea9b84d423d1c5d98db16f2f2594064e
joefreeman8 <joefreeman8@gmail.com>
2024-06-28 10:50:48 +0100

    successfully added post functionality, tested in postman

--- octopus/views.py
+++ octopus/views.py
@@ -11,3 +11,13 @@ class OctopusListView(APIView):
         octopus = Octopus.objects.all()
         serialized_octopus = OctopusSerializer(octopus, many=True)
         return Response(serialized_octopus.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        octopus_to_add = OctopusSerializer(data=request.data)
+        try:
+            octopus_to_add.is_valid()
+            octopus_to_add.save()
+            return Response(octopus_to_add.data, status=status.HTTP_201_CREATED)
+        except Exception as e: 
+            print('Error')
+            return Response(e.__dict__ if e.__dict__ else str(e), status=status.HTTP_422_UNPROCESSABLE_ENTITY)
