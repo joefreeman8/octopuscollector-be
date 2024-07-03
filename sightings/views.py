15604b11679b065b40b386aedf8eb16031e87c4f
joefreeman8 <joefreeman8@gmail.com>
2024-07-03 19:57:29 +0100

    added SightingDetailView

--- sightings/views.py
+++ sightings/views.py
@@ -23,3 +23,31 @@ class SightingListView(APIView):
         except Exception as e: 
             print('Error')
             return Response(e.__dict__ if e.__dict__ else str(e), status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+
+class SightingDetailView(APIView):
+
+    def get_sighting(self, pk):
+        try:
+            return Sighting.objects.get(pk=pk)
+        except Sighting.DoesNotExist:
+            raise NotFound(detail="Can't find that sighting")
+        
+    def get(self, _request, pk):
+        sighting = self.get_sighting(pk=pk)
+        serialized_sighting = SightingSerializer(sighting)
+        return Response(serialized_sighting.data, status=status.HTTP_200_OK)
+    
+    def put(self, request, pk):
+        sighting_to_edit = self.get_sighting(pk=pk)
+        updated_sighting = SightingSerializer(sighting_to_edit, data=request.data)
+
+        if updated_sighting.is_valid():
+            updated_sighting.save()
+            return Response(updated_sighting.data, status=status.HTTP_202_ACCEPTED)
+        
+        return Response(updated_sighting.errors, status=status.HTTP_422_UNPROCESSABLE_ENTITY)
+    
+    def delete(self, request, pk):
+        sighting_to_delete = self.get_sighting(pk=pk)
+        sighting_to_delete.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
